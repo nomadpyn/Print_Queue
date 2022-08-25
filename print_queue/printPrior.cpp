b785621bab20475282d4c23ee18d3b6c6cf10f43
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-08-25 11:09:29 +0700

    Commit 4
    Make constructor with param
    Make destructor

--- print_queue/printPrior.cpp
+++ print_queue/printPrior.cpp
@@ -1,1 +1,16 @@
-#include "printPrior.h"
+﻿#include "printPrior.h"
+#include<iostream>
+#include<string>
+
+// конструктор с параметром
+printPrior::printPrior(int q) {
+	maxQ = q;
+	Wait = new string[q];
+	Pri = new int[q];
+	length = 0;
+}
+// деструктор
+printPrior::~printPrior() {
+	delete[] Wait;
+	delete[] Pri;
+}
