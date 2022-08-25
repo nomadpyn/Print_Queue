26ffeae2be24cfedb1a37e293da824eae19d61f4
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-08-25 10:50:41 +0700

    Commit 2
    Create Class printPrior with printPrior.cpp & printPrior.h

--- print_queue/main.cpp
+++ print_queue/main.cpp
@@ -1,1 +1,11 @@
-﻿
+﻿#include<iostream>
+#include<string>
+#include<ctime>
+#include"printPrior.h"
+
+using namespace std;
+
+int main() {
+	
+	return 0;
+}
---
+++ print_queue/printPrior.h
@@ -0,0 +1,13 @@
+#pragma once
+#include<string>
+
+using namespace std;
+
+class printPrior
+{
+	string* Wait{};
+	int* Pri{};
+	int maxQ{};
+	int length{};
+};
+
