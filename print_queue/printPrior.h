ed162729ee8e46c06fe270ddc256446d256f11c0
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-08-25 11:00:53 +0700

    Commit 3
    Update printPrior.h

--- print_queue/printPrior.h
+++ print_queue/printPrior.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include<string>
 
 using namespace std;
@@ -9,5 +9,21 @@ class printPrior
 	int* Pri{};
 	int maxQ{};
 	int length{};
+public:
+// конструктор	
+	printPrior(int q);
+// конструктор по умолчанию
+	printPrior() :printPrior(10) {};
+// деструктор
+	~printPrior();
+// описание методов класса
+	void Show();
+	void Clear();
+	bool isEmpty() const;
+	bool isFull() const;
+	int GetCount() const;
+	void Add(string per, int pr);
+	string Extract();
+
 };
 
