27b31375f566a9adfe9609c57742ad53680131b4
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2022-08-26 10:33:30 +0700

    Commit 8
    Add struct for statistic
    Add func to get current time

--- print_queue/main.cpp
+++ print_queue/main.cpp
@@ -1,11 +1,29 @@
-﻿#include<iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
 #include<string>
 #include<ctime>
+#include<vector>
 #include"printPrior.h"
+
 
 using namespace std;
 
+struct task {
+	string name{};
+	int time[2]{ 0 };
+};
+
+void getTime(int& hour, int& min) {
+	time_t rawtime;
+	struct tm* timeinfo;
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+	hour = timeinfo->tm_hour;
+	min = timeinfo->tm_min;
+}
+
 int main() {
+
 
 	return 0;
 }
